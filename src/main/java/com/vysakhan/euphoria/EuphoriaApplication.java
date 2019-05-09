e1c043a9fc21e0af0ed1e83ff424ca9082f74de9
Vysakhan Kasthuri <skvysakh@gmail.com>
2019-05-09 11:39:14 +0530

    Spring Initializer

---
+++ src/main/java/com/vysakhan/euphoria/EuphoriaApplication.java
@@ -0,0 +1,13 @@
+package com.vysakhan.euphoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuphoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuphoriaApplication.class, args);
+	}
+
+}
