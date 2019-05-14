0e22b1942769a08e8a25416622865e17d6260204
VYSAKHAN SREEKUMARAN KASTHURI <skvysakh@gmail.com>
2019-05-14 19:01:16 +0000

    wildfly test

--- src/main/java/com/vysakhan/euphoria/EuphoriaApplication.java
+++ src/main/java/com/vysakhan/euphoria/EuphoriaApplication.java
@@ -2,16 +2,23 @@ package com.vysakhan.euphoria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class EuphoriaApplication {
+public class EuphoriaApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EuphoriaApplication.class, args);
 	}
+	
+	 @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EuphoriaApplication.class);
+    }
 	
 	@GetMapping("/")
 	public String getCookBook(){
